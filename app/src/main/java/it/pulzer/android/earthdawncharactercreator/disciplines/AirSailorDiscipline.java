bc6391873d21bc549caf3d10f95bb4fb1f5f6a6a
Thomas Pulzer <t.pulzer@kniel.de>
2017-07-17 13:23:47 +0200

    Added more disciplines.

---
+++ app/src/main/java/it/pulzer/android/earthdawncharactercreator/disciplines/AirSailorDiscipline.java
@@ -0,0 +1,55 @@
+package it.pulzer.android.earthdawncharactercreator.disciplines;
+
+import it.pulzer.android.earthdawncharactercreator.Attribute;
+import it.pulzer.android.earthdawncharactercreator.KarmaModification;
+
+/**
+ * Created by thopu on 11.07.17.
+ */
+
+public class AirSailorDiscipline extends BaseDiscipline {
+
+    public AirSailorDiscipline() {
+        super();
+        importantAttributes.add(Attribute.CHA);
+        importantAttributes.add(Attribute.DEX);
+        importantAttributes.add(Attribute.WILL);
+
+        karmaModifications.put(1, new KarmaModification(1, "Action on an air ship per round"));
+        karmaModifications.put(3, new KarmaModification(1, "Initiative tests"));
+        karmaModifications.put(5, new KarmaModification(1, "Interaction tests"));
+
+        increasedDurability[0] = 5;
+        increasedDurability[1] = 6;
+    }
+
+    @Override
+    public int getPhysicalDefenseModification(int circle) {
+        int modification = 0;
+        if(circle >= 2) {
+            modification += 1;
+        }
+        if(circle >= 6) {
+            modification += 2;
+        }
+        if(circle >= 8) {
+            modification += 3;
+        }
+        return modification;
+    }
+
+    @Override
+    public int getSocialDefenseModification(int circle) {
+        return circle>=4 ? 1 : 0;
+    }
+
+    @Override
+    public int getRecoveryCountModification(int circle) {
+        return circle>=7 ? 1 : 0;
+    }
+
+    @Override
+    public KarmaModification getKarmaModification(int circle) {
+        return karmaModifications.get(circle, null);
+    }
+}
---
+++ app/src/main/java/it/pulzer/android/earthdawncharactercreator/disciplines/ScoutDiscipline.java
@@ -0,0 +1,54 @@
+package it.pulzer.android.earthdawncharactercreator.disciplines;
+
+import it.pulzer.android.earthdawncharactercreator.Attribute;
+import it.pulzer.android.earthdawncharactercreator.KarmaModification;
+
+/**
+ * Created by thopu on 11.07.17.
+ */
+
+public class ScoutDiscipline extends BaseDiscipline {
+
+    public ScoutDiscipline() {
+        super();
+        importantAttributes.add(Attribute.DEX);
+        importantAttributes.add(Attribute.PERC);
+
+        karmaModifications.put(1, new KarmaModification(1, "Test for finding something"));
+        karmaModifications.put(3, new KarmaModification(1, "Initiative tests"));
+        karmaModifications.put(5, new KarmaModification(1, "Recovery tests"));
+
+        increasedDurability[0] = 5;
+        increasedDurability[1] = 6;
+    }
+
+    @Override
+    public int getPhysicalDefenseModification(int circle) {
+        int modification = 0;
+        if(circle >= 2) {
+            modification += 1;
+        }
+        if(circle >= 6) {
+            modification += 2;
+        }
+        if(circle >= 8) {
+            modification += 3;
+        }
+        return modification;
+    }
+
+    @Override
+    public int getMysticalDefenseModification(int circle) {
+        return circle>=4 ? 1 : 0;
+    }
+
+    @Override
+    public int getInitiativeModification(int circle) {
+        return circle>=7 ? 1 : 0;
+    }
+
+    @Override
+    public KarmaModification getKarmaModification(int circle) {
+        return karmaModifications.get(circle, null);
+    }
+}
